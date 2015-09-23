11521e2a43d8d7924be09742b1812420ed54036a
Oksana Serdiuk <okserdiuk@gmail.com>
2015-09-23 16:10:16 +0300

    mapper manager

---
+++ BBL/MapperManager.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BBL
+{
+    public static class MapperManager
+    {
+        public static List<R> GetViewListOfEntity<I, R>(List<I> entities)
+        {
+            List<R> result = new List<R>();
+            try
+            {
+                result = Mapper.Map<IEnumerable<I>, List<R>>(entities);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return result;
+        }
+
+        public static R Map<I, R>(I source)
+        {
+            return Mapper.Map<I, R>(source);
+        }
+    }
+}
