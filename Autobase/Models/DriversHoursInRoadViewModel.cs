205e848b287f96151730ea80522503ff99cbf68f
Oksana <oserdiuk@outlook.com>
2015-12-13 21:00:43 +0200

    done done

---
+++ Autobase/Models/DriversHoursInRoadViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Autobase.Models
+{
+    public class DriversHoursInRoadViewModel
+    {
+        public int DriverId { get; set; }
+        public string DriverFirstName { get; set; }
+        public string DriverSecondName { get; set; }
+        public Dictionary<string, long> DriverDirectionsHours { get; set; }
+        public Dictionary<string, long> DriverCarsHours { get; set; }
+        public TimeSpan AllHoursInRoad { get; set; }
+    }
+}
